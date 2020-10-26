005b242bb6f3494a9af954446948a2a3de8872f1
ARedondoVilla <alexredondovilla@gmail.com>
2020-10-26 16:31:39 +0000

    logo star wars

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 export default function(props) {
     return (
         <nav class="navbar navbar-light bg-light container">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">
+                <img src="https://static-mh.content.disney.io/starwars/assets/navigation/sw_logo_stacked-336c62367939.png"/>
+            </a>
             <form class="form-inline">
                 <button class="btn btn-outline-success" type="button">Main button</button>
             </form>
